23d8e33fc3d521a60c1ac95929ea3c30981c053d
youngfromseoul <youngfromseoul@gmail.com>
2020-09-06 17:22:24 +0900

    first app

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -12,13 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    @IBAction func showAlert(_ sender: Any) {
+        let alert = UIAlertController(title: "Hello", message: "First My APP", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    @IBAction func challenge(_ sender: Any) {
+        let alert = UIAlertController(title: "도전!", message: "도전 너무 재밌다~~", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
-    @IBAction func hello(_ sender: Any) {
-        print("hello")
-    }
-    
-
 }
 
