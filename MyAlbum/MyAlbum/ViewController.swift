96cb5add4a9a0cf183543ffabae5613f0c191b15
youngfromseoul <youngfromseoul@gmail.com>
2020-09-06 11:51:18 +0900

    first app

---
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by hyoyoung.kim on 2020/09/06.
+//  Copyright © 2020 com.hyoyoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
