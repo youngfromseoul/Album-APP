fe905fe22011c71870f332fc4527c918ff6554b2
hyoyoung.kim <freey@freeyui-MacBook-Pro.local>
2020-09-09 22:41:42 +0900

    while study update

---
+++ flowControl.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// -- While
+
+
+//var i = 0
+//while i < 10 {
+//    print(i)
+//    i += 1
+//}
+
+
+print("---while")
+var i = 10
+while i < 10 {
+    print(i)
+    i += 1
+}
+// 조건 > 코드 수행 > 조건 > 코드 수행
+
+print("---Repeat")
+i = 10
+repeat {
+    print(i)
+    i += 1
+} while i < 10
+// 코드 수행 > 조건 > 코드 수행 > 조건
