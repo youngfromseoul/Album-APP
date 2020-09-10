6609abb7800b1b3f7a3006c6ea0a0a72b4c1bf76
hyoyoung.kim <youngfromseoul@kakao.com>
2020-09-10 21:47:54 +0900

    swift_loop

--- flowControl.playground/Contents.swift
+++ flowControl.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Foundation
 
 // -- While
 
@@ -26,3 +27,52 @@ repeat {
 } while i < 10
 // 코드 수행 > 조건 > 코드 수행 > 조건
 //test
+
+
+// -- For Loop
+
+let closedRange = 0...10
+let halfClosedRange = 0..<10
+
+var sum = 0
+for i in halfClosedRange {
+    print("--->\(i)")
+    sum += i
+}
+
+print("---> total sum: \(sum)")
+
+var sinValue: CGFloat = 0
+
+for i in closedRange {
+    sinValue = sin(CGFloat.pi/4 * CGFloat(i))
+}
+
+
+let name = "kim"
+for _ in closedRange {
+    print("-->\(name)")
+}
+
+for i in closedRange {
+    if i % 2 == 0 {
+        print("---> 짝수 : \(i)")
+    }
+}
+
+
+for i in closedRange where i % 2 == 0 {
+    print ("--> 짝수 \(i)")
+}
+
+for i in closedRange {
+    if i != 3 {
+        print ("-->\(i)")
+    }
+}
+
+for i in closedRange {
+    for j in closedRange {
+        print ("gugu -> \(i) * \(j) = \(i * j)")
+    }
+}
